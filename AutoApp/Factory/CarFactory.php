23a0dfb536784ea6df861ac870633bd4102c2a6e
Darko <darkohrnjak06@gmail.com>
2025-12-01 18:49:20 +0100

    dodan factory

---
+++ AutoApp/Factory/CarFactory.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace AutoApp\Factory;
+
+use AutoApp\Models\Car;
+use AutoApp\Models\CarType;
+use AutoApp\Models\CarCollection;
+use AutoApp\Observers\ObserverInterface;
+
+class CarFactory{
+    private CarCollection $carCollection;
+
+    private array $observers = [];
+
+    private int $idCounter = 1;
+
+    public function __construct(){
+        $this->carCollection = new CarCollection();
+    }
+
+    public function attachObserver(ObserverInterface $obs){
+        $this->observers[] = $obs;
+    }
+
+    private function notifyObservers(string $msg){
+        foreach($this->observers as $obs){
+            $obs->update($msg);
+        }
+    }
+
+    public function createCar(string $naziv, int $godiste, string $tipNaziv):Car{
+       
+        $tip = new CarType(rand(1,100), $tipNaziv);
+
+        $car = new Car(
+            $this->idCounter++,
+            $naziv,
+            $godiste,
+            $tip
+        );
+
+        $this->carCollection->add($car);
+        $this->notifyObservers("Proizveden novi auto: {$car->info()}");
+
+        return $car;
+    }
+
+    public function getCars(): CarCollection{
+        return $this->carCollection;
+    }
+}
+
+
+
+?>
