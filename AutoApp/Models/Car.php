9d90e2e4f251781cddc8a4d8771e8332cfa48d6b
Darko <darkohrnjak06@gmail.com>
2025-12-01 18:27:53 +0100

    Dodan je model

---
+++ AutoApp/Models/Car.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace AutoApp\Models;
+
+class Car{
+    public function __construct(
+        public int $id,
+        public string $naziv,
+        public int $godiste,
+        public CarType $tip
+    )
+    {}
+
+    public function info():string{
+        return "{$this->id} - {$this->naziv}, ({$this->godiste}), tip {$this->tip->naziv}";
+    }
+}
+
+
+
+?>
---
+++ AutoApp/Models/CarType.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace AutoApp\Models;
+
+class CarType{
+
+    public function __construct( 
+    public int $id,
+    public string $naziv
+    ){}
+}
+
+
+?>
